232f7a2ca5bb11130444b631c4aa2e4f1cbe9fb5
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-11-23 22:43:34 +0600

    6 List, Create API View

--- status/urls.py
+++ status/urls.py
@@ -4,6 +4,8 @@ from status import views
 app_name = 'status'
 
 urlpatterns = [
-    path('api/', views.StatusAPIView.as_view(), name='api'),
+    path('status/', views.StatusAPIView.as_view(), name='api'),
+    path('status/list/', views.StatusListAPIView.as_view(), name='list_api'),
+    path('status/create/', views.StatusCreateAPIView.as_view()),
 
 ]
--- status/views.py
+++ status/views.py
@@ -6,6 +6,9 @@ from .serializers import StatusSerializer # serializer based on status model
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from rest_framework import generics
+
+
 # Create your views here.
 class StatusAPIView(APIView):
 
@@ -13,3 +16,11 @@ class StatusAPIView(APIView):
         status_list = Status.objects.all()
         status_serializer = StatusSerializer(status_list, many=True)
         return Response(status_serializer.data)
+
+class StatusListAPIView(generics.ListAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
+class StatusCreateAPIView(generics.CreateAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
